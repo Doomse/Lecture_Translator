e4eee890921ca8c23e60944e537d1125ecf2ecac
Doomse <ueqnn@student.kit.edu>
2021-10-08 20:24:18 +0200

    Cleanup work
    Update urls
    Remove unused code
    Register models to admin

--- LT_UI/users/urls.py
+++ LT_UI/users/urls.py
@@ -3,8 +3,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    #path('register/', views.RegisterView.as_view(), name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logged_out.html'), name='logout'),
     path('user/', views.UserView.as_view(), name='user'),
 ]
--- LT_UI/users/views.py
+++ LT_UI/users/views.py
@@ -1,9 +1,8 @@
 from django import forms, urls
 from django.shortcuts import render
-#from django.contrib.messages import views as message_views
 from django.contrib.auth import mixins
 from django.views import generic
-from . import models, forms
+from . import models
 
 # Create your views here.
 class UserView(mixins.LoginRequiredMixin, generic.DetailView):
@@ -13,11 +12,3 @@ class UserView(mixins.LoginRequiredMixin, generic.DetailView):
 
     def get_object(self, queryset=None):
         return self.request.user
-
-
-#class RegisterView(message_views.SuccessMessageMixin, generic.CreateView):
-    
-#    template_name = 'users/register.html'
-#    success_url = urls.reverse_lazy('login')
-#    form_class = forms.UserRegisterForm
-#    success_message = "Your account was created successfully"
