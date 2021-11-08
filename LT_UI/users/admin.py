4dd3fab0a5963298b70ac898f67fee3399af38b3
Doomse <ueqnn@student.kit.edu>
2021-11-08 17:35:51 +0100

    Update admin.py

--- LT_UI/users/admin.py
+++ LT_UI/users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth import admin as auth_admin
 from . import models
 
 
 # Register your models here.
-admin.site.register(models.User)
+admin.site.register(models.User, auth_admin.UserAdmin)
