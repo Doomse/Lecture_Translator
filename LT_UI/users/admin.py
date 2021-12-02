4437556540aa631bac9c28358cbe8fc4c830e55a
Doomse <ueqnn@student.kit.edu>
2021-12-02 12:49:58 +0100

    Add code model for user verification

--- LT_UI/users/admin.py
+++ LT_UI/users/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
+from django.contrib import auth
 from django.contrib.auth import admin as auth_admin
 from . import models
 
 
+class CodeAdmin(admin.ModelAdmin):
+
+    list_display = ['timeout']
+
+    def add_view(self, *args, **kwargs):
+        self.fields = ['code', 'timeout']
+        return super().add_view(*args, **kwargs)
+
+    def change_view(self, *args, **kwargs):
+        self.fields = ['timeout']
+        return super().change_view(*args, **kwargs)
+
+
 # Register your models here.
 admin.site.register(models.User, auth_admin.UserAdmin)
+admin.site.register(models.Code, CodeAdmin)
